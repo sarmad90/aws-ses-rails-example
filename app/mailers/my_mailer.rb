e20c00123a4b767d9815fd8d0c25bfdf79c5caba
Sarmad Sabih <sam_sarmad@hotmail.com>
2016-06-01 23:37:33 +0500

    progress 1

--- app/mailers/my_mailer.rb
+++ app/mailers/my_mailer.rb
@@ -1,2 +1,10 @@
 class MyMailer < ApplicationMailer
+
+  def send_email(options={})
+    @name = options[:name]
+    @email = options[:email]
+    @message = options[:message]
+    mail(:to=>"sarmadserioussam@gmail.com", :subject=>"Amazon SES Email")
+  end
+
 end
